b0fb4f752c21880bc9a7bbda550f6c027567aa6b
aya-cs22 <aya786930@gmail.com>
2023-11-29 09:08:14 +0200

    print the last digit of the number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,16 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 print ("Last digit of " + str(number) +" is" ,end = " ")
 if number > 0 or number == 0:
-	last_digit = number % 10
-	if last_digit == 0:
-		print(str(last_digit) + " and is 0")
-	elif last_digit > 5:
-		print(str(last_digit) +" and is greater than 5")
-	elif last_digit < 6 and last_digit != 0:
-		print (str(last_digit) + " and is less than 6 and not 0")
-	elif last_digit < 6 and last_digit != 0:
-		print (str(last_digit) + " and is less than 6 and not 0")
+    last_digit = number % 10
+    if last_digit == 0:
+        print(str(last_digit) + " and is 0")
+    elif last_digit > 5:
+        print(str(last_digit) +" and is greater than 5")
+    elif last_digit < 6 and last_digit != 0:
+        print (str(last_digit) + " and is less than 6 and not 0")
+    elif last_digit < 6 and last_digit != 0:
+        print (str(last_digit) + " and is less than 6 and not 0")
 else:
-	last_digit = (number % -10) * -1
-	if last_digit > 0 and last_digit != 0:
-		print ("-" +str(last_digit) + " and is less than 6 and not 0")
+    last_digit = (number % -10) * -1
+    if last_digit > 0 and last_digit != 0:
+        print ("-" +str(last_digit) + " and is less than 6 and not 0")
