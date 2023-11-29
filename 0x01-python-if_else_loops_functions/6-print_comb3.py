a51ca03cddd86f2235470db598a6128d91462d63
aya-cs22 <aya786930@gmail.com>
2023-11-29 14:59:54 +0200

    prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 num1 = 0
 num2 =1
-while (num1 <= 8):
-    print (num1)
+while num1 <= 8:
+    num2 = 1
+    
+    while num2 <= 9 :
+        if num2 != num1:
+            print (num1, end="")
+            print (num2, end=", ")
+        num2+=1
     num1+=1
