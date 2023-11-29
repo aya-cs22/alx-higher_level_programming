b15c3f888714216faeef73789341e9ab1c0b1ae0
aya-cs22 <aya786930@gmail.com>
2023-11-29 13:13:44 +0200

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 num = 0
 while num < 100:
-    print(f"{str(num).zfill(2)}".format(num), end="")
+    print(f"{str(num).zfill(2)}".format(num), end=" ")
     if num != 99:
         print(", " ,end ="")
     num += 1
