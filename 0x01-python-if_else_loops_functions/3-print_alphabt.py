bc6d76332f207630202cf2d78c801d57f4255e5e
aya-cs22 <aya786930@gmail.com>
2023-11-29 11:31:32 +0200

    prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,7 @@
 char = 'a'
 while char <= 'z':
     if char == 'q' or char == 'e':
+        char = chr(ord(char) + 1)
         continue
     print(f"{char}".format(char), end="")
     char = chr(ord(char) + 1)
