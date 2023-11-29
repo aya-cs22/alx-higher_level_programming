0374ca84c04662777d9696fa24de91590bfe7017
aya-cs22 <aya786930@gmail.com>
2023-11-29 11:30:21 +0200

    prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,6 @@
 char = 'a'
 while char <= 'z':
     if char == 'q' or char == 'e':
-        char = chr(ord(char) + 1)
         continue
     print(f"{char}".format(char), end="")
     char = chr(ord(char) + 1)
