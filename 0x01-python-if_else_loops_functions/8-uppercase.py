145799cbd37906ca09f8b3dddac83557c5fe088c
aya-cs22 <aya786930@gmail.com>
2023-11-30 10:00:45 +0200

    prints a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for word in str:
-        if (str != " " or str != ","):
-            print(f"{str.capitalize()}".format(str))
+    print(f"{str.capitalize()}".format(str))
