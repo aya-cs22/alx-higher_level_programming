53433f062105dc8e9c01d72d8b2b3303eb2c1aa7
aya-cs22 <aya786930@gmail.com>
2023-11-30 10:57:35 +0200

    prints a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     result = ""
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            result += chr(ord(i) - ord('a') + ord ('A'))
+            result += chr(ord(i) - ord('a') + ord('A'))
         else:
             result += i
     print(f"{result}".format(result))
