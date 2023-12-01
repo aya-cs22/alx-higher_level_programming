9a029b6f3322c1b76b07a8dd4262054bfb9022dd
aya-cs22 <aya786930@gmail.com>
2023-12-01 11:46:36 +0200

    functions from the file calculator_1.py, does some Maths, and prints the result.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print(f"{a} + {b} = {add(a,b)}".format(a, b, add(a, b)))
-    print(f"{a} + {b} = {sub(a,b)}".format(a, b, add(a, b)))
-    print(f"{a} + {b} = {mul(a,b)}".format(a, b, add(a, b)))
-    print(f"{a} + {b} = {div(a,b)}".format(a, b, add(a, b)))
+    print(f"{a} - {b} = {sub(a,b)}".format(a, b, sub(a, b)))
+    print(f"{a} * {b} = {mul(a,b)}".format(a, b, mul(a, b)))
+    print(f"{a} / {b} = {div(a,b)}".format(a, b, div(a, b)))
