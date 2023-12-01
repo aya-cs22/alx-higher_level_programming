920efb55131f86b3bcfa25f63bac11fd1844c665
aya-cs22 <aya786930@gmail.com>
2023-12-01 11:25:48 +0200

    prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import add_0
-a = 1
-b = 2
-add_0.add(a,b)
-print(f"{a} + {b} = {add_0.add(a,b)}".format(a,b,add_0.add(a,b)))
+    import add_0
+    a = 1
+    b = 2
+    add_0.add(a,b)
+    print(f"{a} + {b} = {add_0.add(a,b)}".format(a,b,add_0.add(a,b)))
