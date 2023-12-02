7299be1661bd13fd2039aed094edb08655243b08
aya-cs22 <aya786930@gmail.com>
2023-12-02 22:00:30 +0200

    prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    c = len(argv) - 1
+    if (c == 0)
+        print("0 arguments.")
+    elif (c == 1)
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else
+        print("{} arguments:".format(len(argv)))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
