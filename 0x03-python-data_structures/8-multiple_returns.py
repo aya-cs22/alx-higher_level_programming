d1f9ab1876aec4d42c381eea3db79cd7ea01f17a
aya-cs22 <aya786930@gmail.com>
2023-12-06 18:46:14 +0200

    a function that returns a tuple with the length of a string and its first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    length = len(sentence)
-    if sentence == None or len(sentence) == 0:
-        first_char = None
+#!/usr/bin/pesycode
+def magical_returns(words):
+    sparkle = len(words)
+    if words is None or len(words) == 0:
+        enchantment = None
     else:
-        first_char = sentence[0]
-    return length, first_char
+        enchantment = words[0]
+    return sparkle, enchantment
