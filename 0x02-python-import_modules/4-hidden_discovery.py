71acb59123db8f2f2f586f686d927ff8a3d818c3
aya-cs22 <aya786930@gmail.com>
2023-12-06 09:24:37 +0200

    prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in range(0, len(dir(hidden_4))):
-        if dir(hidden_4)[i][0:2] != "__":
-            print("{}".format(dir(hidden_4)[i]))
+
+    for x in dir(hidden_4):
+        if x[:2] != '__':
+            print(x)
