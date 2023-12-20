afca5999be381eba97b0d9f42809b99335a922a3
aya-cs22 <aya786930@gmail.com>
2023-12-20 21:41:35 +0200

    function that prints x elements of a list.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    i, c = 0, 0
     while x > i:
         try:
             print("{:d}".format(my_list[i]), end="")
-            i += 1
+            c += 1
         except (ValueError, TypeError):
             pass
+        i += 1
     print()
     return i
