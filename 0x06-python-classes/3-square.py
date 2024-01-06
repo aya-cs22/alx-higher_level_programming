2cb00ea3c818b5548076b3b472d966311bf1c677
aya-cs22 <aya786930@gmail.com>
2024-01-06 22:48:48 +0200

    a function that divides element by element 2 lists.

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -20,5 +20,5 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        def area(self):
-            return self.__size
+    def area(self):
+        return self.__size
