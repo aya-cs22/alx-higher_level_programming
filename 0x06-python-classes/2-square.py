fa8b0702460bb4a3c6016282cd2f7f7781f284de
aya-cs22 <aya786930@gmail.com>
2024-01-06 22:37:24 +0200

    a function that divides element by element 2 lists.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,8 +14,8 @@ class Square:
             TypeError: if size is not an integer.
             ValueError: if size is less than Zero.
         """
-        self.__size = size
         if not isinstance(size ,int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
