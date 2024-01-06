d370007ab1bedd8c8536bd48144ff950a5529f24
aya-cs22 <aya786930@gmail.com>
2024-01-06 22:43:04 +0200

    a function that divides element by element 2 lists.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """Square module"""
+
+
 class Square:
     """Defines a square"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Constructor
 
         Args:
-            size: length of a side of the square.
+            size (int): Length of a side of the square.
         Raises:
-            TypeError: if size is not an integer.
-            ValueError: if size is less than Zero.
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
         """
-        if not isinstance(size ,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
