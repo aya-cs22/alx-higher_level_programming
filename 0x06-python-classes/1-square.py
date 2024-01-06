83b0bd10db0aa210be6e0cb4adabde241d9cc499
aya-cs22 <aya786930@gmail.com>
2024-01-06 22:27:03 +0200

    a function that divides element by element 2 lists.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """Defines a square"""
     def __init__(self, size):
+        """Constructor
+
+        Args:
+            size: length of a side of the square.
+        """
         self.__size = size
         
