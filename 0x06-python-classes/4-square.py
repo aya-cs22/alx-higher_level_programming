5e0d77aaa7c3e1c745ac40735f6de1d512e55a6c
aya-cs22 <aya786930@gmail.com>
2024-01-08 13:20:47 +0200

    Classes and Objects

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size (int): Length of a side of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
+        """
+        @property
+        def size(self):
+            return self.__size
+        @size.setter
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+        def area(self):
+            return self.__size * self.__size
