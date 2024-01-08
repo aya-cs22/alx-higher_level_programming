00e9cb48d5bbf75ad57dac3dcd839dee41d44c0a
aya-cs22 <aya786930@gmail.com>
2024-01-08 15:58:23 +0200

    class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,6 +14,7 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than zero.
         """
+        self.__size = size
 
         @property
         def size(self):
