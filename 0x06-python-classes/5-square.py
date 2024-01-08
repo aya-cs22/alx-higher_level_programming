7df8e57de3daa55cb062d3913eec292de39f38c3
aya-cs22 <aya786930@gmail.com>
2024-01-08 15:45:05 +0200

    class

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,3 +4,20 @@
 
 class squar:
     """Defines a square"""
+
+
+    def __init__(self, size=0):
+        """Constructor
+        Args:
+            size (int): Length of a side of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
+        """
+        @property 
+        def size(self):
+            return self.__size
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
