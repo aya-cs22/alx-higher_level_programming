77fcd3aeb0cc51dd74e8822dbe7574be107c78fa
aya-cs22 <aya786930@gmail.com>
2024-01-12 00:00:56 +0200

    More Classes and Objects

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""module for Rectangle class"""
+
+
+class Rectangle:
+    """define the Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the Rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get for the private instance attr width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set for the private instance attr width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """get for the private instance attr height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set for the private instance attr height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        else:
+            return (self.height + self.width) * 2
