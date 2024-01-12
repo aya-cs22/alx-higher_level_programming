4859011f5fd5a507b5ae0dac10152e911a99488c
aya-cs22 <aya786930@gmail.com>
2024-01-12 15:03:16 +0200

    More Classes and Objects

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
-class Rectangle:
-    """class that does nothing"""
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+"""Square generation module for Python project 0x06
+"""
 
-    @property
-    def width(self):
-        return self.__width
 
-    @property
-    def height(self):
-        return self.__height
+class Square:
+    """Class defined for square generation.
 
-    @width.setter
-    def width(self, value):
-        if isinstance(value, int) and value >= 0:
-            self.__width = value
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            raise TypeError("width must be an integer")
+    Args:
+        size (int): length of one side of square
 
-    @height.setter
-    def height(self, value):
-        if isinstance(value, int) and value >= 0:
-            self.__height = value
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            raise TypeError("height must be an integer")
+    Attributes:
+        __size (int): length of one side of square
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
     def area(self):
-        return self.width * self.height
+        """Calulates area of square.
 
-    def perimeter(self):
-        if self.width == 0 or self.height == 0:
-            return 0
-        else:
-            return (self.width + self.height) * 2
+        Attributes:
+            __size (int): length of one side of square
 
-    def __str__(self):
-        string = ""
-        if self.width == 0 or self.height == 0:
-            return ""
-        for i in range(0, self.height):
-            string += '#' * self.width
-            if i < self.height - 1:
-                string += '\n'
-        return string
+        Returns:
+            area (int): length of one side, squared
 
-    def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        """
+        area = self.__size * self.__size
+        return area
