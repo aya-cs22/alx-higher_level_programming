1e74b0743de039bfc13fcd9c652d0baa3d69ccbf
aya-cs22 <aya786930@gmail.com>
2024-01-12 21:58:55 +0200

    More Classes and Objects

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -57,9 +57,9 @@ class Rectangle:
         string = ""
         if self.width == 0 or self.height == 0:
             return ""
-        for i in range(0, self.height):
+        for row in range(0, self.height):
             string += str(self.print_symbol) * self.width
-            if i < self.height - 1:
+            if row < self.height - 1:
                 string += '\n'
         return string
 
@@ -73,8 +73,9 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_1, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
+
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
