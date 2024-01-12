a47e09f9e01b5dcc6dab0d6394a72fc5d5be64a5
aya-cs22 <aya786930@gmail.com>
2024-01-12 16:12:39 +0200

    More Classes and Objects

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Define the Rectangle."""
+
+
 class Rectangle:
     """Define the Rectangle."""
-
     def __init__(self, width=0, height=0):
         """Initializes the Rectangle."""
         self.width = width
@@ -11,42 +13,51 @@ class Rectangle:
     def width(self):
         """Get for the private instance attribute width."""
         return self.__width
+
     @width.setter
     def width(self, value):
         """Set for the private instance attribute width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
     @property
     def height(self):
         """Get for the private instance attribute height."""
         return self.__height
+
     @height.setter
     def height(self, value):
         """Set for the private instance attribute height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
+
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
         else:
             return (self.__width + self.__height) * 2
+
     def __str__(self):
         str = ""
         if self.width == 0 or self.height == 0:
             return ""
         for row in range(self.height):
-            str += "#" * self.width + '\n'
+            str += '#' * self.width
+            if row < self.height - 1:
+                str += '\n'
         return str
+
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
