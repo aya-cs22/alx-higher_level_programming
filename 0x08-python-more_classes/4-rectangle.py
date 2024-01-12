7fb3c77f962be6529c51c6e0b3c7171667f587ab
aya-cs22 <aya786930@gmail.com>
2024-01-12 14:18:58 +0200

    More Classes and Objects

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -34,23 +34,19 @@ class Rectangle:
         else:
             self.__height = value
     def area(self):
-        return self.width * self.height
-
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.__height == 0 or self.__width == 0:
             return 0
         else:
-            return (self.width + self.height) * 2
-
+            return (self.__width + self.__height) * 2
     def __str__(self):
-        string = ""
+        str = ""
         if self.width == 0 or self.height == 0:
             return ""
-        for i in range(0, self.height):
-            string += '#' * self.width
-            if i < self.height - 1:
-                string += '\n'
-        return string
-
+        for row in range(self.height):
+            str += "#" * self.width + '\n'
+        return str
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
