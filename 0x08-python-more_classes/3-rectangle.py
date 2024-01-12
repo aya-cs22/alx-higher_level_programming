1e20d1c87cb733c824bcf3f73b8f356a2e07eeb5
aya-cs22 <aya786930@gmail.com>
2024-01-12 15:22:09 +0200

    More Classes and Objects

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -14,12 +14,13 @@ class Rectangle:
         return self.__width
     @width.setter
     def width(self, value):
+        """Set for the private instance attribute width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
-
+        else:
+            self.__width = value
     @property
     def height(self):
         """Get/set the height of the Rectangle."""
