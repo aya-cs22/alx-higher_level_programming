4ed06090fc09e6df4ae177f092a9496cc683ed9c
aya-cs22 <aya786930@gmail.com>
2024-01-12 20:13:20 +0200

    More Classes and Objects

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -67,5 +67,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
