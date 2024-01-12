3f38f6330d2d4e6fce5d1315a795e8da06e24dd4
aya-cs22 <aya786930@gmail.com>
2024-01-12 15:07:38 +0200

    More Classes and Objects

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,38 +1,52 @@
 #!/usr/bin/python3
-"""Square generation module for Python project 0x06
-"""
+class Rectangle:
+    """Define the Rectangle."""
 
+    def __init__(self, width=0, height=0):
+        """Initializes the Rectangle."""
+        self.width = width
+        self.height = height
 
-class Square:
-    """Class defined for square generation.
-
-    Args:
-        size (int): length of one side of square
-
-    Attributes:
-        __size (int): length of one side of square
-
-    Raises:
-        TypeError: if size is not an integer
-        ValueError: if size is less than 0
-
-    """
-    def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
-
+    @property
+    def width(self):
+        """Get for the private instance attribute width."""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """Set for the private instance attribute width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    @property
+    def height(self):
+        """Get for the private instance attribute height."""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """Set for the private instance attribute height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
     def area(self):
-        """Calulates area of square.
-
-        Attributes:
-            __size (int): length of one side of square
-
-        Returns:
-            area (int): length of one side, squared
-
-        """
-        area = self.__size * self.__size
-        return area
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+    def __str__(self):
+        str = ""
+        if self.width == 0 or self.height == 0:
+            return ""
+        for row in range(self.height):
+            str += "#" * self.width + '\n'
+        return str
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
