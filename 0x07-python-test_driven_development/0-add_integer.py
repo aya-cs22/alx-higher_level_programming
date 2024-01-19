aac2fdba02b029a39b237d145edaedd45b9f573c
aya-cs22 <aya786930@gmail.com>
2024-01-19 17:44:51 +0200

    0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+"""Module for add a+b method."""
+def add_integer(a, b=98):
+    """"Args:
+    a: an integer number
+    b: an integer number
+
+    Raise:
+    TypeError: if a, b are not int or float"""
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, int):
+        b =int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
