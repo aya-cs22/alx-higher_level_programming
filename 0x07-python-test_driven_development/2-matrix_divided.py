e89bd6b4c6ffd4ea84abbc19744b07e37d9f1366
aya-cs22 <aya786930@gmail.com>
2024-01-20 11:22:01 +0200

    0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module for add a+b method."""
+
+def matrix_divided(matrix, div):
+    """Divides all elements of matrix by div.
+    Args:
+        matrix: List of lists containing int or float
+        div: number to divide matrix by
+    Returns:
+        list: List of lists representing divided matrix.
+    """
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
+        raise TypeError("All elements of the matrix must be integers or floats")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    divided_matrix = []
+    for row in matrix:
+        new_row = [round(element / div, 2) for element in row]
+        divided_matrix.append(new_row)
+
+    return divided_matrix
