868cc5a01d20c0691f9a0c7af4d6c37848ebcce9
aya-cs22 <aya786930@gmail.com>
2024-01-20 18:32:53 +0200

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Module for  prints a square with the character #"""
+"""
+print strings till . or ? or :
 
+return Nothing
+"""
 
+#hello.
 def text_indentation(text):
-    """text indent"""
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    for char in text:
-        if char in [".", "?", ":"]:
+    length = 0
+    current = 0
+    for value in (text):
+        print(value, end="")
+        if value in {'.', ';', '?'}:
             print()
             print()
-        print(char, end="")
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")      
