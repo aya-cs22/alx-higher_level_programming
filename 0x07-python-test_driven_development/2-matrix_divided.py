f73ee1767995c40754d97e08a36dfd4e90c44dea
aya-cs22 <aya786930@gmail.com>
2024-01-20 14:42:30 +0200

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for add a+b method."""
 
+
 def matrix_divided(matrix, div):
     """Divides all elements of matrix by div.
     Args:
@@ -10,10 +11,14 @@ def matrix_divided(matrix, div):
         list: List of lists representing divided matrix.
     """
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
 
-    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
-        raise TypeError("All elements of the matrix must be integers or floats")
+    for row in matrix:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("All elements of the matrix must be " +
+                                "integers or floats")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
