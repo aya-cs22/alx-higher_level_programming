8701e3675e722859918a3b7bdf51b38947c44222
aya-cs22 <aya786930@gmail.com>
2024-01-29 10:19:11 +0200

    Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,4 +5,5 @@
 def is_kind_of_class(obj, a_class):
     """"check if obj is subclass of a_class"""
     if type(obj) is a_class:
-        return isinstance(obj, a_class)
+        return True
+    return False
