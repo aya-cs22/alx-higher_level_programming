5b0c0ea88154004db3e7720d4e13596d5f80b1d4
aya-cs22 <aya786930@gmail.com>
2024-01-29 10:13:35 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""Module with the method lookpu"""
+
+
+def is_kind_of_class(obj, a_class):
+    """"check if obj is subclass of a_class"""
+def is_kind_of_class(obj, a_class):
+    if type (obj) is a_class:
+        return True
+    return False
