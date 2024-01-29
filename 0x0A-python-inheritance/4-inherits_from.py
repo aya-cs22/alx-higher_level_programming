a6a3d831c10c069bd1f7d8432e57e0a168eb38d1
aya-cs22 <aya786930@gmail.com>
2024-01-29 10:51:35 +0200

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    '''check if obj is subclass of a_class'''
+    '''Determines if an object is a true subclass of a_class.'''
     for subclass in obj.__class__.__mro__[1:]:
         if subclass == a_class:
             return True
