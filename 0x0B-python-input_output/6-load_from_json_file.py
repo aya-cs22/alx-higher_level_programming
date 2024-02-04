d9e6a1844970a4fb4287fb8776dc0790a395512d
aya-cs22 <aya786930@gmail.com>
2024-02-04 19:23:38 +0200

    Input/Output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-'''a function that creates an Object'''
-
-
+'''module for load_from_json_file method'''
 import json
 
 
 def load_from_json_file(filename):
-    """"creates an Object from a “JSON file”"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(filename)
-    except FileNotFoundError:
-        return None
-
+    '''function that creates an Object from a JSON file'''
+    with open(filename, 'r', encoding='UTF8') as file:
+        return json.load(file)
