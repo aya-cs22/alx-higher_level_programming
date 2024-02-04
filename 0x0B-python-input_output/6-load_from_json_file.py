d58f2bb1ab64ea77e1c1527a25ee94d389160d79
aya-cs22 <aya786930@gmail.com>
2024-02-04 19:09:46 +0200

    Input/Output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,3 +9,6 @@ def load_from_json_file(filename):
     """"creates an Object from a “JSON file”"""
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(filename)
+    except FileNotFoundError:
+        return None
+
