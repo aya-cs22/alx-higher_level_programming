e27bd7a1c874763fa1fe0b2f095f312f9609eb3d
aya-cs22 <aya786930@gmail.com>
2024-02-04 16:54:52 +0200

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """"that reads a text file"""
-    with open("UTF8", "r") as file:
-        data = file.read()
-    print(data)
+    with open(str(filename), "r") as file:
+        for line in file:
+            print(line, end="")
