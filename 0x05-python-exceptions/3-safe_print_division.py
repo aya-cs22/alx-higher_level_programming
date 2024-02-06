630b79a3e55486c4207ebd88c8a39d1fa5c91495
aya-cs22 <aya786930@gmail.com>
2024-02-06 11:21:50 +0200

    zerodivion

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,7 @@ def safe_print_division(a, b):
     try:
         res = a/b
     except ZeroDivisionError:
+        res = None
         return("None")
     finally:
         print("Inside result: {}".format(res))
