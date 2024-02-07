98fdbf7c018058185bcb67bfb2f98019767331e7
aya-cs22 <aya786930@gmail.com>
2024-02-07 12:52:58 +0200

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""Defines a class squar"""
+""""Defines a class square"""
 
 
 class Square:
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,17 @@
 #!/bin/usr/python3
-""""square module"""
+""""Defines aclass square"""
 
 
 class Square:
-    """""""
+    """"
+    class that defines a square by: (based on 0-square.py)
+    Attributes:
+    size: size of a square (1 side).
+    """
+    def __init__(self, size):
+        """"
+        creat a new instance of square (1 side)
+        Args:
+        size: length of a side of the square.
+        """
+        self.__size = size
