72cb49d0ceaf955e87da3dde667d588856ebd9ff
aya-cs22 <aya786930@gmail.com>
2024-02-09 17:34:16 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,13 +6,13 @@ class Base:
     '''A class to set self id'''
 
     __nb_objects = 0
+
     def __init__(self, id=None):
         '''instatiation
         Args: the id: None is default value'''
 
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        
