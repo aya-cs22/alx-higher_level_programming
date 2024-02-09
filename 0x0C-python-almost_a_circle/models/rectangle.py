8300f0e33824ebe5184e4fdc4f6a1cce2660bf7f
aya-cs22 <aya786930@gmail.com>
2024-02-09 17:38:59 +0200

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/in/python3
+'''A module to write the class Rectangle that inherits from Base'''
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''A subclass of Base Class'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Instatiation of a new subclass rectangle
+        Args:
+        width and height and x and y and id
+        '''
+
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
