e52bc5daf9fc56d2addbf3b3ff9fba71418495f9
aya-cs22 <aya786930@gmail.com>
2024-02-09 19:04:17 +0200

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-'''A module to write the class Rectangle that inherits from Base'''
-
+"""class Rectangle"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''A subclass of Base Class'''
+    '''A subclass of Base class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Instatiation of a new subclass rectangle
@@ -15,7 +14,39 @@ class Rectangle(Base):
         '''
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+
+        @property
+        def width(self):
+            '''width method'''
+            return self.__width
+
+        @width.setter
+        def width(self, width):
+            self.__width = width
+
+        @property
+        def height(self):
+            '''height method'''
+            return self.__height
+
+        @height.setter
+        def height(self, height):
+            self.__height = height
+
+        @property
+        def x(self):
+            '''x method'''
+            return self.__x
+
+        @x.setter
+        def x(self, x):
+            self.__x = x
+
+        @property
+        def y(self):
+            '''y method'''
+            return self.__x
+
+        @y.setter
+        def y(self, y):
+            self.__y = y
