c906d76b0650762c3277e2fa7c82d15bd49fc124
aya-cs22 <aya786930@gmail.com>
2024-03-13 23:02:11 +0200

    a script that prints a script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const c = 'C is fun';
-const python = 'python is cool';
+const python = 'Python is cool';
 const js = 'JavaScript is amazing';
 console.log(c);
 console.log(python);
