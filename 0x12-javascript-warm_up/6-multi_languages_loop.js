bb5b0e05e2d8ab3643ab53f9a296ecfa841bd58f
aya-cs22 <aya786930@gmail.com>
2024-03-15 11:06:24 +0200

    using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const c = ['c is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+for (i = 0; i < 3; i++) {
+  console.log(c[i]);
+}
--- 0x12-javascript-warm_up/argument.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-for (let i = 0; i < process.argv.length; i++) {
-    console.log(process.argv[i]);
-  }
-  
