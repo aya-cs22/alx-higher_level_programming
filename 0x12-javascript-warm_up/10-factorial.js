c87208c2eb68aa992965144dae6c507eaca3c831
aya-cs22 <aya786930@gmail.com>
2024-03-15 16:57:23 +0200

    computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// const args = process.argv;
+let factorial = 1;
+let i;
+const num1 = parseFloat(process.argv[2]);
+if (num1 === 2) {
+  console.log('Missing size');
+} else {
+  for (i = 1; i <= process.argv[2]; i++) {
+    factorial *= i;
+  }
+  console.log(factorial);
+}
