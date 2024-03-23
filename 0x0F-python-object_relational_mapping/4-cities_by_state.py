7a008bbdc571b934de3049fa2a4cacf85ba1c2e9
aya-cs22 <aya786930@gmail.com>
2024-03-23 13:53:50 +0200

    lists all cities from the database hbtn_0e_4_usa0

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db_access = MySQLdb.connect(port=3306, host="localhost",
+                                user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db_access.cursor()
+    cur.execute("""SELECT * FROM cities ORDER BY cities.id ASC
+                """)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db_access.close()
