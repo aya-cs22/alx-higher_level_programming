d24260dc70a6810a45600d6ad08be32b38df505b
aya-cs22 <aya786930@gmail.com>
2024-03-23 12:03:12 +0200

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    db_access = MySQLdb.connect(port=3306, host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    db_access = MySQLdb.connect(port=3306, host="localhost",
+                                user=argv[1], passwd=argv[2], db=argv[3])
     cur = db_access.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
