9dbcf8526f986d40d68eef701a029164ce9a2b89
aya-cs22 <aya786930@gmail.com>
2024-03-23 13:33:44 +0200

    arguments and displays all values in the states table of hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+""" arguments and displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 But this time, write one that is safe from MySQL injections!"""
 
 
