1a7374c26ee7e5b5d20b3344cca860aee7c41dc0
aya-cs22 <aya786930@gmail.com>
2024-03-25 14:16:24 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Start link class to table in database """
 
-
+from model_state import Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
@@ -13,4 +13,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, primary_key=True, nullable=False)
+    state_id = Column(Integer, foreign_key=True, nullable=False)
