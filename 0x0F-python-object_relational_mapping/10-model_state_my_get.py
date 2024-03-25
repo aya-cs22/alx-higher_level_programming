cb78f204b14aa1a453e61ad2134980958219d379
aya-cs22 <aya786930@gmail.com>
2024-03-25 11:43:56 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,14 +7,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
     if state:
         print(f'{state.id}')
     else:
-        print("Nothing")
+        print('Not found')
     session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    _states = session.query(State).order_by(State.id)
-    for state in _states:
+    states = session.query(State).filter(State.name.like('%a%'))
+    for state in states:
         print(f'{state.id}: {state.name}')
     session.close()
