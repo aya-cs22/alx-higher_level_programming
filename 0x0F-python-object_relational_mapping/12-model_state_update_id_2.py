d19643426b70007ca0e84fa3a722c18cd705f829
aya-cs22 <aya786930@gmail.com>
2024-03-25 13:24:43 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.id == 2)
+    states = session.query(State).filter(State.id == 2).first()
     states.name = "New Mexico"
     session.commit()
     session.close()
