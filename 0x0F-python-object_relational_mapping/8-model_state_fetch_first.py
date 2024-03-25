edbbf45ceba0a37c8234bd0b80918efb94860df8
aya-cs22 <aya786930@gmail.com>
2024-03-25 09:57:52 +0200

    a script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""a script that prints the first State object from the database hbtn_0e_6_usa"""
-
-
+"""a script that prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import Base, State
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}@localhost/{}'.format(sys.argv[1], sys.argv[2]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).first()
-if not states:
-    print("Nothing")
-else:
-    for state in states:
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if state:
         print(f'{state.id}: {state.name}')
-session.close()
+    else:
+        print("Nothing")
+    session.close()
