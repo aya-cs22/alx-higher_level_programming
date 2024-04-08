d697a3efa2759eac53533fcee5e7c93bb1d081b0
aya-cs22 <aya786930@gmail.com>
2024-04-08 21:08:23 +0200

    Add Node.js script to print X based on a given size

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const args = process.argv;
 let i, x;
-if (args.length === 2) {
+if (args.length < 3 || isNaN(Number(args[2]))) {
   console.log('Missing size');
 } else if (args.length === 3) {
-  for (i = 0; i < args[2]; i++) {
-    let row = '';
-    for (x = 0; x < args[2]; x++) {
-      row += 'X';
+    for (i = 0; i < args[2]; i++) {
+      let row = '';
+      for (x = 0; x < args[2]; x++) {
+        row += 'X';
     }
     console.log(row);
   }
