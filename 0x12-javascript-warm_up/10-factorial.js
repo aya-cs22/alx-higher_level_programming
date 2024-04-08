79ab2312b26c785ece2d9427a400ca19ad70dd1e
aya-cs22 <aya786930@gmail.com>
2024-04-08 21:39:52 +0200

    Add Node.js script to calculate factorial of a given number

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,13 +2,14 @@
 const args = process.argv;
 const n = parseInt(args[2]);
 function factorial (n) {
-  if (isNaN(n)) {
-    return NaN;
-  } else if (n === 0 || n === 1) {
+  if (n === 0 || n === 1) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
 }
-
-console.log(factorial(n));
+if (args.length < 3) {
+  process.exit(1);
+} else {
+  console.log(factorial(n));
+}
