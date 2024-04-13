95c9f1d299844a0a6e618385a2147c6a8576c3e3
aya-cs22 <aya786930@gmail.com>
2024-04-13 17:10:08 +0200

    this module lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """this module lists all states from the database hbtn_0e_0_usa"""
-
-
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
     db_access = MySQLdb.connect(port=3306, host="localhost",
                                 user=argv[1], passwd=argv[2], db=argv[3])
