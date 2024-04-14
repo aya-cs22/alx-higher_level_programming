3c75815ffcebab5cbe001676126a06943013e2ff
aya-cs22 <aya786930@gmail.com>
2024-04-14 16:44:07 +0200

    a script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State)  
+    states = session.query(State)
     for state in states:
         print(f'{state.id}: {state.name}')
     session.close()
