b236d5b674513c8d64cb45e1246d67643c0121dd
aya-cs22 <aya786930@gmail.com>
2024-04-26 14:48:33 +0300

    finds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""finds a peak in a list of unsorted integers"""
+"""A module finds a peak in a list of unsorted integers"""
 def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
     max = list_of_integers[0]
