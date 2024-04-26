5f88f9745a96fde9572c48123b596275f7d1b24d
aya-cs22 <aya786930@gmail.com>
2024-04-26 19:32:52 +0300

    Net work

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-# -d => To send Form Data to the web server when a POST request is made
 curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
