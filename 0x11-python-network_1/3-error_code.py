5677532b52e9068599918d873d1fb7d179abe67e
aya-cs22 <aya786930@gmail.com>
2024-04-27 15:31:14 +0300

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Error code"""
+import urllib
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print('urllib.error.HTTPError'.format(e.code))
