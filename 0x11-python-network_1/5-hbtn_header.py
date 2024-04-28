1c3d2c926a5b916840d54db984d6d634c456b257
aya-cs22 <aya786930@gmail.com>
2024-04-28 15:20:31 +0300

    Response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """Response header value"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    head = requests.headers.get("X-Request-Id")
+    print(head)
