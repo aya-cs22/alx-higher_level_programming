694e1d4d3e7dde20971784f005185dc9fead45a6
aya-cs22 <aya786930@gmail.com>
2024-04-28 15:54:24 +0300

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""POST an email"""
-import requests
+"""Sends a POST request to a given URL with a given email."""
 import sys
+import requests
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    response = requests.post(url, data=value)
-    print(response.text)
+
+    r = requests.post(url, data=value)
+    print(r.text)
+# #!/usr/bin/python3
+# """POST an email"""
+# import requests
+# import sys
+# if __name__ == "__main__":
+#     url = sys.argv[1]
+#     value = {"email": sys.argv[2]}
+#     response = requests.post(url, data=value)
+#     print(response.text)
