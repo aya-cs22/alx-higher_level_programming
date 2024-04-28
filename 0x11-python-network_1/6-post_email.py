06d91e4b621e7af40a9dae1ab069ad5df3da0503
aya-cs22 <aya786930@gmail.com>
2024-04-28 15:41:30 +0300

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """POST an email"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    response = requests.post(url, data = value)
+    print('Your email is: {}'.format(response))
