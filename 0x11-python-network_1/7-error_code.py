b540ca12675651e336d6eeb6a2a25b7c12afa1c9
aya-cs22 <aya786930@gmail.com>
2024-04-28 16:14:50 +0300

    sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a Python script that takes in a URL, sends a request to the URL and displays the body of the response"""
+"""sends a request to the URL and displays the body of the response"""
 import requests
 import sys
 if __name__ == "__main__":
