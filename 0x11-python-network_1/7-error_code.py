deb1fe218c1b0ae00e6ad653af1baa0bb91ff4c0
aya-cs22 <aya786930@gmail.com>
2024-04-28 16:12:49 +0300

    sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print('Error code:{}'.format(response.status_code))
+        print('Error code: {}'.format(response.status_code))
     else:
-        response.text
+        print(response.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter and sends a POST request to URL with the letter as a parameter."""
