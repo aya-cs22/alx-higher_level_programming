00b5a1dfa44d8b3b192d167e052c96ecab03d981
aya-cs22 <aya786930@gmail.com>
2024-04-28 21:27:44 +0300

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""a Python script that takes in a letter and sends a POST request to URL with the letter as a parameter."""
-import sys
+"""Search API"""
 import requests
-
+import sys
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if (len(sys.argv[1] == 1)):
         q = ""
     else:
         q = sys.argv[1]
-
     value = {"q": q}
-
-    response = requests.post("http://0.0.0.0:5000/search_user", json=value)
-
+    response = requests.post("http://0.0.0.0:5000/search_user",json = value)
     try:
-        response_json = response.json()
+        response_json = response.json
         if isinstance(response_json, dict):
             if response_json:
                 print("[{}] {}".format(response_json['id'], response_json['name']))
@@ -23,4 +19,4 @@ if __name__ == "__main__":
         else:
             print("Not a valid JSON")
     except ValueError:
-        print('Not a valid JSON')
+        print("Not a valid JSON")
