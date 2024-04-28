94f55336f7a2ba7e717495a32fff648d3a7a5f86
aya-cs22 <aya786930@gmail.com>
2024-04-28 16:33:15 +0300

    a Python script that takes in a letter and sends a POST request to URL with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 """a Python script that takes in a letter and sends a POST request to URL with the letter as a parameter."""
+import sys
+import requests
+import json
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"q": sys.argv[2]}
+
+    response = requests.post("http://0.0.0.0:5000/search_user", json=value)
+
+    try:
+        response_json = response.json()
+        if response_json:
+            for item in response_json:
+                print("[{}] {}".format(item['id'], item['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print('Not a valid JSON')
