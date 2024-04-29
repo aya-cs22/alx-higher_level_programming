dbe135c509376b86a5ad7103e052073d16b06f16
aya-cs22 <aya786930@gmail.com>
2024-04-29 21:43:26 +0300

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-"""my status"""
-import requests
-if __name__ == "__main__":
+'''
+a Python script that fetches https://alx-intranet.hbtn.io/status
+'''
+
+
+if _name_ == "_main_":
+    import requests
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    print("Body response:")
+    res = response.text
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+# #!/usr/bin/python3
+# """my status"""
+# import requests
+# if __name__ == "__main__":
+#     url = "https://alx-intranet.hbtn.io/status"
+#     response = requests.get(url)
+#     print('Body response:')
+#     print('\t- type: {}'.format(type(response.text)))
+#     print('\t- content: {}'.format(response.text))
