370aa7085944631c5b61cd939e7b8d5a5bb0e3f8
aya-cs22 <aya786930@gmail.com>
2024-06-24 13:34:16 +0300

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
-
+#!/usr/bin/node
+if (process.argv.length === 2 || isNaN(parseInt(process.argv[2]))) {
+  console.log('NaN');
+} else {
+  const num1 = parseInt(process.argv[2]);
+  const num2 = parseInt(process.argv[3]);
+  console.log(num1 + num2);
+}
