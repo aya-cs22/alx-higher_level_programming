55c1d029d14da4b9e1da4cb7650d7416c25e3a0c
aya-cs22 <aya786930@gmail.com>
2024-06-26 18:45:55 +0300

    a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-
-const { dict } = require('./101-data');
-
+const dict = require('./101-data.js').dict;
+const newDict = {};
 for (const key in dict) {
-  if (Object.prototype.hasOwnProperty.call(dict, key)) {
-    console.log(`Key: ${key}, Value: ${dict[key]}`);
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
 }
+console.log(newDict);
