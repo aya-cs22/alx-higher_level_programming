c4ee3461a278c75c7509ea63b65c923ec2d73bf0
aya-cs22 <aya786930@gmail.com>
2024-06-28 13:32:17 +0300

    a script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
-const fs = require('fs');
 
-const url = process.argv[2];
+const request = require('request');
+const URL = process.argv[2];
 const file = process.argv[3];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(file, body, 'utf8', function (err) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(file);
-      }
-    });
+request(URL, (error, response, body) => {
+  if (!error) {
+    const fs = require('fs');
+    fs.writeFileSync(file, body, 'utf8');
   }
 });
