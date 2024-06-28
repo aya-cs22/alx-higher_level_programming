285e3b5ab3b3e1ad4ffca8b01500a19832c9e3f3
aya-cs22 <aya786930@gmail.com>
2024-06-28 11:38:19 +0300

    a script that writes a string to a file my_file.txt

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error, data) {
   if (error) {
     console.log(error);
-  } else {
-    console.log(data);
   }
 });
