cb04a74b64d270b3daf8128f9a6af2117d6093df
aya-cs22 <aya786930@gmail.com>
2024-06-29 12:18:04 +0300

    a script that prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,11 +6,11 @@ const characterID = 18;
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-    return;
+    // return;
   }
   if (response.statusCode !== 200) {
-    console.log('Error: ' + response.statusCode);
-    return;
+    console.log(response.statusCode);
+    // return;
   }
 
   const films = JSON.parse(body).results;
@@ -20,62 +20,10 @@ request(url, function (error, response, body) {
     film.characters.forEach(character => {
       if (character.endsWith(`/${characterID}/`)) {
         count++;
-        return;
+        // return;
       }
     });
   });
 
   console.log(count);
 });
-
-// #!/usr/bin/node
-// const request = require('request');
-// const url = process.argv[2];
-// const characterID = 18;
-// const characterURL = `https://swapi-api.alx-tools.com/api/people/${characterID}/`;
-
-// request(url, function (error, response, body) {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   if (response.statusCode !== 200) {
-//     console.log('Error: ' + response.statusCode);
-//     return;
-//   }
-
-//   const films = JSON.parse(body).results;
-//   let count = 0;
-
-//   films.forEach(film => {
-//     if (film.characters.includes(characterURL)) {
-//       count++;
-//     }
-//   });
-
-//   console.log(count);
-// });
-
-// #!/usr/bin/node
-// const request = require('request');
-// const url = process.argv[2];
-// const characterID = 18;
-// const characterURL = `https://swapi-api.alx-tools.com/api/people/${characterID}/`;
-// request(url, function (error, response, body) {
-//   if (error) {
-//     console.error(error);
-//   } else if (response.statusCode !== 200) {
-//     console.log(response.statusCode);
-//   } else {
-//     const films = JSON.parse(body).results;
-//     let count = 0;
-//     films.forEach(film => {
-//       film.characters.forEach(character => {
-//         if (film.characters.includes(characterURL)) {
-//           count++;
-//         }
-//       });
-//     });
-//     console.log(count);
-//   }
-// });
