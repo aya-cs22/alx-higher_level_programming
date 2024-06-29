05975ae98ab5d8f57bf7094775d8f3be0e806471
aya-cs22 <aya786930@gmail.com>
2024-06-29 12:14:59 +0300

    a script that prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,21 +3,49 @@ const request = require('request');
 const url = process.argv[2];
 const characterID = 18;
 const characterURL = `https://swapi-api.alx-tools.com/api/people/${characterID}/`;
+
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.log(response.statusCode);
-  } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach(film => {
-      film.characters.forEach(character => {
-        if (film.characters.includes(characterURL)) {
-          count++;
-        }
-      });
-    });
-    console.log(count);
+    return;
   }
+  if (response.statusCode !== 200) {
+    console.log('Error: ' + response.statusCode);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  let count = 0;
+
+  films.forEach(film => {
+    if (film.characters.includes(characterURL)) {
+      count++;
+    }
+  });
+
+  console.log(count);
 });
+
+// #!/usr/bin/node
+// const request = require('request');
+// const url = process.argv[2];
+// const characterID = 18;
+// const characterURL = `https://swapi-api.alx-tools.com/api/people/${characterID}/`;
+// request(url, function (error, response, body) {
+//   if (error) {
+//     console.error(error);
+//   } else if (response.statusCode !== 200) {
+//     console.log(response.statusCode);
+//   } else {
+//     const films = JSON.parse(body).results;
+//     let count = 0;
+//     films.forEach(film => {
+//       film.characters.forEach(character => {
+//         if (film.characters.includes(characterURL)) {
+//           count++;
+//         }
+//       });
+//     });
+//     console.log(count);
+//   }
+// });
